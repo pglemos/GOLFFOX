1f2813afabbc1726ac4882dfe9b2b65164fcc6ba
pglemos <pglemos31@gmail.com>
2025-11-18 19:22:13 -0300

    fix: Remove menu hambúrguer duplicado do MobileSidebar em mobile

--- apps/web/components/ui/sidebar.tsx
+++ apps/web/components/ui/sidebar.tsx
@@ -88,7 +88,7 @@ export const SidebarBody = (props: React.ComponentProps<typeof motion.div>) => {
   return (
     <>
       <DesktopSidebar {...props} />
-      <MobileSidebar {...(props as React.ComponentProps<'div'>)} />
+      {/* MobileSidebar removido - usando apenas o botão hambúrguer da Topbar */}
     </>
   );
 };
